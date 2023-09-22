c9393173f1bafda3c7e2c49d53c29e9680ce4d42
mujugen <101939481+mujugen@users.noreply.github.com>
2023-09-23 02:16:52 +0800

    created workout page frontend

--- app/src/main/java/com/mujugen/mypersonaltrainer/WorkoutActivity.kt
+++ app/src/main/java/com/mujugen/mypersonaltrainer/WorkoutActivity.kt
@@ -4,18 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 
 class WorkoutActivity : AppCompatActivity() {
+
+    private val setNumber = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout)
         val exerciseType = intent.getStringExtra("exerciseType")
-        println("welcome to workout activity")
-        println(exerciseType)
 
-        val myLinearLayout = findViewById<TextView>(R.id.textView5)
-        myLinearLayout.text = exerciseType
+        val exerciseTypeText = findViewById<TextView>(R.id.exerciseTypeText)
+        val setText = findViewById<TextView>(R.id.setText)
+        exerciseTypeText.text = exerciseType
+        setText.text = "Set $setNumber"
 
+
+        val loadProgressBar = findViewById<ProgressBar>(R.id.loadProgressBar)
+        loadProgressBar.progress = 70
+
+        val repsProgressBar = findViewById<ProgressBar>(R.id.repsProgressBar)
+        repsProgressBar.progress = 20
     }
 }
