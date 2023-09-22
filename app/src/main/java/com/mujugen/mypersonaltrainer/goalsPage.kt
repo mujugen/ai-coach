9731f8dcebd383782e458d4f4d35c24ee8900f14
mujugen <101939481+mujugen@users.noreply.github.com>
2023-09-23 00:38:35 +0800

    fixed progress bar

--- app/src/main/java/com/mujugen/mypersonaltrainer/goalsPage.kt
+++ app/src/main/java/com/mujugen/mypersonaltrainer/goalsPage.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +36,19 @@ class GoalsPage : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_goals_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val volumeGoalsProgressBar = view.findViewById<ProgressBar>(R.id.volumeGoalsProgressBar)
+        val strengthGoalsProgressBar = view.findViewById<ProgressBar>(R.id.strengthGoalsProgressBar)
+        val consistencyGoalsProgressBar = view.findViewById<ProgressBar>(R.id.consistencyGoalsProgressBar)
+        val weightGoalsProgressBar = view.findViewById<ProgressBar>(R.id.weightGoalsProgressBar)
+        volumeGoalsProgressBar.progress = 50
+        strengthGoalsProgressBar.progress = 80
+        consistencyGoalsProgressBar.progress = 30
+        weightGoalsProgressBar.progress = 90
     }
 
     companion object {
