aaee868aee8871f4a035bd57b851239454a6d661
mujugen <101939481+mujugen@users.noreply.github.com>
2023-09-23 19:50:18 +0800

    goal page functionality

--- app/src/main/java/com/mujugen/mypersonaltrainer/goalsPage.kt
+++ app/src/main/java/com/mujugen/mypersonaltrainer/goalsPage.kt
@@ -1,11 +1,17 @@
 package com.mujugen.mypersonaltrainer
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.ProgressBar
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +27,20 @@ class GoalsPage : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var volumeProgress = 400
+    private var strengthProgress = 320
+    private var consistencyProgress = 8
+    private var currentWeight = 95
+
+    private var startingWeight = 100
+    private var volumeGoal = 800
+    private var strengthGoal = 400
+    private var consistencyGoal = 12
+    private var weightGoal = 85
+    private var weightChangeGoal = kotlin.math.abs(startingWeight - weightGoal)
+    private var weightChangeProgress = kotlin.math.abs(currentWeight - startingWeight)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,12 +65,78 @@ class GoalsPage : Fragment() {
         val strengthGoalsProgressBar = view.findViewById<ProgressBar>(R.id.strengthGoalsProgressBar)
         val consistencyGoalsProgressBar = view.findViewById<ProgressBar>(R.id.consistencyGoalsProgressBar)
         val weightGoalsProgressBar = view.findViewById<ProgressBar>(R.id.weightGoalsProgressBar)
-        volumeGoalsProgressBar.progress = 50
-        strengthGoalsProgressBar.progress = 80
-        consistencyGoalsProgressBar.progress = 30
-        weightGoalsProgressBar.progress = 90
+
+        val volumeGoalsProgressText = view.findViewById<TextView>(R.id.volumeGoalsProgressText)
+        val strengthGoalsProgressText = view.findViewById<TextView>(R.id.strengthGoalsProgressText)
+        val consistencyGoalsProgressText = view.findViewById<TextView>(R.id.consistencyGoalsProgressText)
+        val weightGoalsProgressText = view.findViewById<TextView>(R.id.weightGoalsProgressText)
+
+        volumeGoalsProgressBar.progress = ((volumeProgress.toFloat() / volumeGoal) * 100).toInt()
+        strengthGoalsProgressBar.progress = ((strengthProgress.toFloat() / strengthGoal) * 100).toInt()
+        consistencyGoalsProgressBar.progress = ((consistencyProgress.toFloat() / consistencyGoal) * 100).toInt()
+        weightGoalsProgressBar.progress = ((weightChangeProgress.toFloat() / weightChangeGoal) * 100).toInt()
+        volumeGoalsProgressText.text = "$volumeProgress reps/$volumeGoal reps"
+        strengthGoalsProgressText.text = "$strengthProgress kg/$strengthGoal kg"
+        consistencyGoalsProgressText.text = "$consistencyProgress weeks/$consistencyGoal weeks"
+        weightGoalsProgressText.text = "$currentWeight kg/$weightGoal kg"
+
+
+        val setGoalBtn = view.findViewById<Button>(R.id.setGoalBtn)
+        val confirmGoalBtn = view.findViewById<Button>(R.id.confirmGoalBtn)
+        val setGoalsPopup = view.findViewById<FrameLayout>(R.id.setGoalsPopup)
+        setGoalBtn.setOnClickListener { setGoalsPopup.visibility = View.VISIBLE }
+
+
+        val setGoalsVolumeText = view.findViewById<EditText>(R.id.setGoalsVolumeText)
+        val setGoalsStrengthText = view.findViewById<EditText>(R.id.setGoalsStrengthText)
+        val setGoalsConsistencyText = view.findViewById<EditText>(R.id.setGoalsConsistencyText)
+        val setGoalsWeightText = view.findViewById<EditText>(R.id.setGoalsWeightText)
+
+        // Set the initial text values
+        setGoalsVolumeText.setText(volumeGoal.toString())
+        setGoalsStrengthText.setText(strengthGoal.toString())
+        setGoalsConsistencyText.setText(consistencyGoal.toString())
+        setGoalsWeightText.setText(weightGoal.toString())
+
+        confirmGoalBtn.setOnClickListener {
+            val volumeText = setGoalsVolumeText.text.toString()
+            val strengthText = setGoalsStrengthText.text.toString()
+            val consistencyText = setGoalsConsistencyText.text.toString()
+            val weightText = setGoalsWeightText.text.toString()
+
+            try {
+                volumeGoal = volumeText.toInt()
+                strengthGoal = strengthText.toInt()
+                consistencyGoal = consistencyText.toInt()
+                weightGoal = weightText.toInt()
+
+                weightChangeGoal = kotlin.math.abs(startingWeight - weightGoal)
+                weightChangeProgress = kotlin.math.abs(currentWeight - startingWeight)
+
+
+                volumeGoalsProgressBar.progress = ((volumeProgress.toFloat() / volumeGoal) * 100).toInt()
+                strengthGoalsProgressBar.progress = ((strengthProgress.toFloat() / strengthGoal) * 100).toInt()
+                consistencyGoalsProgressBar.progress = ((consistencyProgress.toFloat() / consistencyGoal) * 100).toInt()
+                weightGoalsProgressBar.progress = ((weightChangeProgress.toFloat() / weightChangeGoal) * 100).toInt()
+
+                volumeGoalsProgressText.text = "$volumeProgress reps/$volumeGoal reps"
+                strengthGoalsProgressText.text = "$strengthProgress kg/$strengthGoal kg"
+                consistencyGoalsProgressText.text = "$consistencyProgress weeks/$consistencyGoal weeks"
+                weightGoalsProgressText.text = "$currentWeight kg/$weightGoal kg"
+
+                hideKeyboard(view)
+
+                setGoalsPopup.visibility = View.INVISIBLE
+            } catch (e: NumberFormatException) {
+            }
+        }
+
     }
 
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
